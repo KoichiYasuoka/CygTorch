9e286484b1ce8b06a702b58288190bd6cfa4d123
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-02-23 18:11:49 +0900

    bug fix

--- installer/sentencepiece.sh
+++ installer/sentencepiece.sh
@@ -2,6 +2,11 @@
 # SentencePiece installer for Cygwin64, which requires:
 #   python37-devel python37-pip python37-cython python37-numpy python37-wheel
 #   gcc-g++ git make cmake
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
 git clone --depth=1 https://github.com/google/sentencepiece
 cd sentencepiece
 cmake . && make
