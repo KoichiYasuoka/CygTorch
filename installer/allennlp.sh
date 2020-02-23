91c6f8b0a724f16f099bfcfdfb6733643b6dd9b6
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-02-23 17:41:30 +0900

    initial release

---
+++ installer/allennlp.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+# Allennlp installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy python37-cffi
+#   gcc-g++ gcc-fortran git wget make cmake
+#   libopenblas liblapack-devel libhdf5-devel libfreetype-devel
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
+pip3.7 install -U cython wheel pybind11 mecab-cygwin
+pip3.7 install 'spacy>=2.2.2' --no-build-isolation
+pip3.7 install cygtorch -f https://github.com/KoichiYasuoka/CygTorch
+pip3.7 install git+https://github.com/scipy/scipy
+wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/sentencepiece.sh
+sh -x sentencepiece.sh
+wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/h5py.sh
+sh -x h5py.sh
+pip3.7 install allennlp 'spacy>=2.2.2'
+exit 0
---
+++ installer/h5py.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# h5py installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy python37-wheel
+#   gcc-g++ git libhdf5-devel
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
+git clone --depth=1 https://github.com/h5py/h5py
+cd h5py
+python3.7 setup.py configure --hdf5-version=1.10.6
+python3.7 setup.py configure --hdf5=/usr/lib/libhd5.dll.a
+python3.7 setup.py bdist_wheel
+cd dist
+pip3.7 install h5py*.whl
+exit 0
