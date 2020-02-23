37e93fe6815d9afb70816fc06111e99fe610d701
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-02-23 15:40:15 +0900

    initial release

---
+++ installer/sentencepiece.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# SentencePiece installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy python37-wheel
+#   gcc-g++ git make cmake
+git clone --depth=1 https://github.com/google/sentencepiece
+cd sentencepiece
+cmake . && make
+cd python
+( echo /version=version,/a
+  echo '    data_files=[("local/bin",['
+  ls ../src/*.dll | sed 's/^.*$/"&",/' ; echo '])],'
+  echo .
+  echo wq ) | ex -s setup.py
+env PKG_CONFIG_PATH=.. CPATH=../src LIBRARY_PATH=../src python3.7 setup.py bdist_wheel
+cd dist
+pip3.7 install sentencepiece*.whl
+exit 0
