6af140f5686ba03ebb3184941aaa9d18b7c12d3e
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-02-23 18:02:27 +0900

    check improved

--- installer/allennlp.sh
+++ installer/allennlp.sh
@@ -11,10 +11,20 @@ esac
 pip3.7 install -U cython wheel pybind11 mecab-cygwin
 pip3.7 install 'spacy>=2.2.2' --no-build-isolation
 pip3.7 install torch -f https://github.com/KoichiYasuoka/CygTorch
-pip3.7 install git+https://github.com/scipy/scipy
-wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/sentencepiece.sh
-sh -x sentencepiece.sh
-wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/h5py.sh
-sh -x h5py.sh
+pip3.7 install scipy@git+https://github.com/scipy/scipy
+pip3.7 list |
+( if egrep '^sentencepiece '
+  then :
+  else wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/sentencepiece.sh
+       sh -x sentencepiece.sh
+  fi
+)
+pip3.7 list |
+( if egrep '^h5py '
+  then :
+  else wget https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/h5py.sh
+       sh -x h5py.sh
+  fi
+)
 pip3.7 install allennlp 'spacy>=2.2.2'
 exit 0
