cecb4e2a8c55eeaffbb31906ac84ee761ddfa8b2
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-03-05 21:43:24 +0900

    initial commit

---
+++ installer/nlpcube.sh
@@ -0,0 +1,17 @@
+#! /bin/sh -x
+# NLP-Cube installer for Cygwin, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy
+#   gcc-g++ gcc-fortran git curl make cmake libopenblas liblapack-devel
+case "`uname -a`" in
+*' Cygwin') : ;;
+*) echo Only for Cygwin >&2
+   exit 2 ;;
+esac
+pip3.7 install -U cython wheel pybind11
+pip3.7 install scipy@git+https://github.com/scipy/scipy
+pip3.7 list |
+( egrep '^dyNET ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/dynet.sh | sh -x
+)
+pip3.7 install nlpcube
+exit 0
