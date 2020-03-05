161c138485d92f6cf7deab6d8d91e281ae6e4a4e
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-03-05 23:01:45 +0900

    Only for Cygwin64

--- installer/nlpcube.sh
+++ installer/nlpcube.sh
@@ -1,10 +1,10 @@
 #! /bin/sh -x
-# NLP-Cube installer for Cygwin, which requires:
+# NLP-Cube installer for Cygwin64, which requires:
 #   python37-devel python37-pip python37-cython python37-numpy
 #   gcc-g++ gcc-fortran git curl make cmake libopenblas liblapack-devel
 case "`uname -a`" in
-*' Cygwin') : ;;
-*) echo Only for Cygwin >&2
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
    exit 2 ;;
 esac
 pip3.7 install -U cython wheel pybind11
