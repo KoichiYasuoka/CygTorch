74be514e0dab63dad64ec1948bd7107552ddca77
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-03-11 23:39:03 +0900

    initial release

---
+++ installer/ja-mecab-udify.sh
@@ -0,0 +1,34 @@
+#! /bin/sh -x
+# ja-mecab-udify installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy python37-cffi
+#   gcc-g++ gcc-fortran git curl make cmake
+#   libopenblas liblapack-devel libhdf5-devel libfreetype-devel libuv-devel
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
+pip3.7 list |
+( egrep '^allennlp ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/allennlp.sh | sh -x
+)
+pip3.7 list |
+( egrep '^pyahocorasick ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/pyahocorasick.sh | sh -x
+)
+pip3.7 list |
+( egrep '^pytokenizations ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/pytokenizations.sh | sh -x
+)
+pip3.7 list |
+( egrep '^tokenizers ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/tokenizers.sh | sh -x
+)
+pip3.7 list |
+( if egrep '^ja-mecab-udify '
+  then :
+  else V=`pip3.7 list | sed -n 's/^tokenizers  *\([^ ]*\)  *$/\1/p'`
+       pip3.7 install tokenizers==$V https://github.com/PKSHATechnology-Research/camphr_models/releases/download/0.5/ja_mecab_udify-0.5.tar.gz
+  fi
+)
+exit 0
