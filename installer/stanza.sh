e9ec5c71463af76dc3b38e287b108638e3f495a5
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-03-21 12:03:10 +0900

    initial release

---
+++ installer/stanza.sh
@@ -0,0 +1,11 @@
+#! /bin/sh -x
+# Stanza installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy gcc-g++
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
+pip3.7 install torch -f https://github.com/KoichiYasuoka/CygTorch
+pip3.7 install stanza
+exit 0
