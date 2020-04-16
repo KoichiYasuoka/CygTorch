464088b65d8ddb6507ea52e463dc0166b83c34ee
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-04-16 23:29:03 +0900

    initial release

---
+++ installer/en-udify.sh
@@ -0,0 +1,38 @@
+#! /bin/sh -x
+# en-udify installer for Cygwin64, which requires:
+#   python37-devel python37-pip python37-cython python37-numpy python37-cffi
+#   gcc-g++ gcc-fortran git curl wget make cmake
+#   libopenblas liblapack-devel libhdf5-devel libfreetype-devel libuv-devel
+case "`uname -a`" in
+*'x86_64 Cygwin') : ;;
+*) echo Only for Cygwin64 >&2
+   exit 2 ;;
+esac
+pip3.7 list |
+( egrep '^allennlp ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/allennlp.sh | sh -x
+)
+pip3.7 install pyahocorasick@git+https://github.com/KoichiYasuoka/pyahocorasick
+pip3.7 list |
+( egrep '^tokenizers ' ||
+  curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/tokenizers.sh | sh -x
+)
+if [ -x /usr/lib/python3.7/site-packages/tokenizations/tokenizations*.dll ]
+then :
+else pip3.7 uninstall pytokenizations
+     curl https://raw.githubusercontent.com/KoichiYasuoka/CygTorch/master/installer/pytokenizations.sh | sh -x
+fi
+pip3.7 install 'fugashi[unidic-lite]'
+pip3.7 list |
+( if egrep '^en-udify '
+  then :
+  else V=`pip3.7 list | sed -n 's/^tokenizers  *\([^ ]*\)  *$/\1/p'`
+       pip3.7 install tokenizers==$V  https://github.com/PKSHATechnology-Research/camphr_models/releases/download/0.5/en_udify-0.5.tar.gz
+  fi
+)
+python3.7 -c '
+import spacy
+ud=spacy.load("en_udify")
+quit()
+'
+exit 0
