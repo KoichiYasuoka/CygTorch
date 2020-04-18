e930bab5caaeb266ff2f69367b21f7652504dafd
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-04-18 23:47:13 +0900

    compile KNP

--- installer/knp.sh
+++ installer/knp.sh
@@ -1,35 +1,55 @@
 #! /bin/sh -x
 # Juman and KNP installer for Cygwin, which requires:
 #   gcc-g++ wget make
-case "`uname -a`" in
-*'x86_64 Cygwin') : ;;
-*) echo Only for Cygwin64 >&2
-   exit 2 ;;
-esac
 D=/tmp/knp$$
 mkdir $D
-if [ ! -x /usr/local/bin/juman.exe ]
+if [ ! -x /usr/local/bin/juman ]
 then cd $D
-     wget 'http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/juman/juman-7.01.tar.bz2' -O juman.tar.gz
-     tar jxf juman.tar.gz
+     wget http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/juman/juman-7.01.tar.bz2
+     tar xjf juman-7.01.tar.bz2
      cd juman-7.01
      ./configure
+     make || exit 1
      make install
 fi
 if [ ! -x /usr/local/bin/knp ]
-then if [ ! -x /cygdrive/?/Program?Files/knp/knp.exe ]
-     then cd $D
-	  wget 'http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/juman/juman-7.0-x64-installer.exe' -O juman-installer.exe
-	  wget 'http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.11-x64-installer.exe' -O knp-installer.exe
-	  chmod u+x juman-installer.exe knp-installer.exe
-	  ./juman-installer.exe /VERYSILENT
-	  ./knp-installer.exe /VERYSILENT
-     fi
-     cat > /usr/local/bin/knp << 'EOF'
-#! /bin/sh
-iconv -f utf-8 -t cp932 | exec /cygdrive/?/Program?Files/knp/knp.exe "$@" | iconv -f cp932 -t utf-8
+then cd $D
+     wget http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.19.tar.bz2
+     tar xjf knp-4.19.tar.bz2
+     cd knp-4.19/CRF++-0.58
+     if egrep '^#ifndef __CYGWIN__' winmain.h
+     then :
+     else ex -s winmain.h << 'EOF'
+/#define  *main/i
+#ifndef __CYGWIN__
+.
+$a
+#endif
+.
+wq
 EOF
-     chmod 755 /usr/local/bin/knp
+     fi
+     cd ../dict/ebcf
+     if [ ! -s cf.idx ]
+     then for F in cf.idx.xz cf.dat.xz.1 cf.dat.xz.2
+          do wget https://github.com/KoichiYasuoka/knp-cygwin64/raw/master/share/knp/dict/ebcf/$F
+          done
+          cat cf.dat.xz.1 cf.dat.xz.2 > cf.dat.xz
+          touch cf.idx.xz
+          unxz cf.idx.xz cf.dat.xz
+     fi
+     if [ ! -s noun.idx ]
+     then for F in noun.idx.xz noun.dat.xz.1 noun.dat.xz.2
+          do wget https://github.com/KoichiYasuoka/knp-cygwin64/raw/master/share/knp/dict/ebcf/$F
+          done
+          cat noun.dat.xz.1 noun.dat.xz.2 > noun.dat.xz
+          touch noun.idx.xz
+          unxz noun.idx.xz noun.dat.xz
+     fi
+     cd ../..
+     ./configure
+     make || exit 1
+     make install
 fi
 rm -fr $D
 exit 0
