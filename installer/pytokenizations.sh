a6099b12c38651aa727f5d0c670ebce6d2b88b00
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-05-26 18:46:25 +0900

    version 0.4.10

--- installer/pytokenizations.sh
+++ installer/pytokenizations.sh
@@ -17,8 +17,9 @@ export PATH USERPROFILE
 wget https://static.rust-lang.org/rustup/dist/"$C"-pc-windows-gnu/rustup-init.exe
 chmod u+x rustup-init.exe
 ./rustup-init.exe -y --no-modify-path --default-host "$C"-pc-windows-gnu --default-toolchain nightly --profile minimal
-git clone --depth=1 https://github.com/tamuhey/tokenizations
-cd tokenizations/python
+wget https://github.com/tamuhey/tokenizations/archive/python/0.4.10.tar.gz
+tar xzf 0.4.10.tar.gz
+cd tokenizations-python*/python
 cargo build --release
 ( B=`cygpath -ad /usr/bin | sed 's/\\\\/\\\\\\\\\\\\\\\\/g'`
   for PYO in $D/.cargo/registry/src/*/pyo3-0*
@@ -44,6 +45,7 @@ rm -fr target/release/build/pyo3-*
 cargo build --release
 cp target/release/tokenizations.dll tokenizations/tokenizations.dll
 chmod 755 tokenizations/tokenizations.dll
-pip3.7 install .
+pip3.7 install poetry
+pip3.7 install . --no-build-isolation
 rm -fr $D
 exit 0
