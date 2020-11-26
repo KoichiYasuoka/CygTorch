c178fc97deb43f6af0d155142dd6c4c8ccf7e35c
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-11-26 09:01:20 +0900

    tokenizers==0.8.1

--- installer/tokenizers.sh
+++ installer/tokenizers.sh
@@ -13,32 +13,26 @@ mkdir $D
 cd $D
 PATH="$D/.cargo/bin:$PATH"
 USERPROFILE="`cygpath -ad $D`"
-PYTHONIOENCODING=utf-8
-PYTHON_SYS_EXECUTABLE="`cygpath -ad /usr/bin/python3.7`"
-export PATH USERPROFILE PYTHONIOENCODING PYTHON_SYS_EXECUTABLE
+PYO3_PYTHON="`cygpath -ad /usr/bin/python3.7`"
+PYTHON_SYS_EXECUTABLE="$PYO3_PYTHON"
+export PATH USERPROFILE PYO3_PYTHON PYTHON_SYS_EXECUTABLE
 wget https://static.rust-lang.org/rustup/dist/"$C"-pc-windows-gnu/rustup-init.exe
 chmod u+x rustup-init.exe
-./rustup-init.exe -y --no-modify-path --default-host "$C"-pc-windows-gnu --default-toolchain nightly --profile minimal
-wget https://github.com/huggingface/tokenizers/archive/python-v0.7.0.tar.gz
-tar xzf python-v0.7.0.tar.gz
-cd tokenizers-python-v0.7.0/bindings/python
+./rustup-init.exe -y --no-modify-path --default-host "$C"-pc-windows-gnu --default-toolchain stable --profile minimal
+wget https://github.com/huggingface/tokenizers/archive/python-v0.8.1.tar.gz
+tar xzf python-v0.8.1.tar.gz
+cd tokenizers-python-v0.8.1/bindings/python
 cargo build --release
 ( B=`cygpath -ad /usr/bin | sed 's/\\\\/\\\\\\\\\\\\\\\\/g'`
   for PYO in $D/.cargo/registry/src/*/pyo3-0*
   do cd $PYO
-     ( cat << 'EOF'
-s?^/*??
-.+1,/^}/s?^?//?
-/pythonXY/s/pythonXY:.*/pythonXY:python3.7m"/
-.+1s?^?//?
-/=native={}\\\\libs"/s/=native={}.*$/=native={}"/
-.+1,/);/s?^?//?
-EOF
+     ( echo '%s/"python{}{}"/"python{}.{}m"/'
        echo '%s/=native={}".*$/=native='$B'");/'
        echo wq
      ) | ex -s build.rs
      ex -s src/exceptions.rs << 'EOF'
 %s/"windows"/"dummy"/
+%s/(all(windows/(all(dummy/
 wq
 EOF
   done
@@ -51,11 +45,25 @@ EOF
 %s/ asm!/ llvm_asm!/
 wq
 EOF
+  cd $D/.cargo/registry/src/*/onig_sys-*
+  ex -s build.rs << 'EOF'
+/let mut cc/a
+    cc.define("ONIG_NO_PRINT", Some("1"));
+.
+wq
+EOF
+  for F in oniguruma/src/config.h.win*
+  do ex -s $F << 'EOF'
+/^#define uid_t/d
+/^#define gid_t/d
+wq
+EOF
+  done
 )
-rm -fr target/release/build/pyo3-* target/release/build/parking_lot-*
+rm -fr target/release/build/pyo3-* target/release/build/parking_lot-* target/release/build/onig_sys-*
 cargo build --release
-pip3.7 install git+https://github.com/PyO3/setuptools-rust
-python3.7 setup.py bdist_wheel
+pip3.7 install setuptools-rust@git+https://github.com/PyO3/setuptools-rust
+python3.7m.exe setup.py bdist_wheel
 cd dist
 pip3.7 install tokenizers*.whl
 rm -fr $D
