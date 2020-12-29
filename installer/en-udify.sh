a43ba42302f47e9d8224cd395e63e12aaf2ef246
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-12-29 17:32:01 +0900

    sentencepiece>=0.1.94

--- installer/en-udify.sh
+++ installer/en-udify.sh
@@ -30,7 +30,8 @@ pip3.7 list |
 ( if egrep '^en-udify +0.7'
   then :
   else V=`pip3.7 list | sed -n 's/^tokenizers  *\([^ ]*\) *$/\1/p'`
-       pip3.7 install tokenizers==$V https://github.com/PKSHATechnology-Research/camphr_models/releases/download/0.7.0/en_udify-0.7.tar.gz 'camphr>=0.7.2'
+       W=`pip3.7 list | sed -n 's/^transformers  *\([^ ]*\) *$/\1/p'`
+       pip3.7 install tokenizers==$V transformers==$W https://github.com/PKSHATechnology-Research/camphr_models/releases/download/0.7.0/en_udify-0.7.tar.gz 'camphr>=0.7.2'
   fi
 )
 ex -s /usr/lib/python3.7/site-packages/udify/modules/bert_pretrained.py << 'EOF'
